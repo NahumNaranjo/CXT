5d7ffb0db8680b686d35748505fd304554211b5f
Nahum <toodoo1703@gmail.com>
2026-03-24 18:06:29 -0600

    first commit

---
+++ readFile.h
@@ -0,0 +1,11 @@
+#ifndef READFILE_H
+#define READFILE_H
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include "hermes.h"
+    #include "map.h"
+    #include "txtFunctions.h"
+    char* readFile(const char *file, size_t *size_out);
+    AnalyzeTextData* parseReportFiles(char *content);
+    void* separateFiles(const char *file, size_t *size_out);
+#endif
---
+++ txtFunctions.h
@@ -0,0 +1,25 @@
+#ifndef TXTFUNCTIONS_H
+#define TXTFUNCTIONS_H
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdbool.h>
+    #include <stddef.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #include "hermes.h"
+    #include "generalData.h"
+
+    //counts all words in the txt file
+    size_t countWords(char *txt);
+    //counts new lines
+    size_t countNewLines(char *txt);
+    //finds most common words in the file
+    commonWordsData mostCommonWord(char *txt);
+    //! UNUSED FUNCTION
+    /*
+    //checks if the word 
+    bool compareWords(char **token, Map *wordMap, size_t **size);
+    */
+    //analyzes the text and returns both data
+    AnalyzeTextData* AnalyzeText(char *txt, size_t *size);
+#endif
